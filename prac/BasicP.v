769a4b9d05ccee90ff72340cb4ef8f8f55adea76
Sean <sean8purdue@gmail.com>
2018-08-28 21:29:50 -0400

    andb3

--- prac/BasicP.v
+++ prac/BasicP.v
@@ -90,15 +90,19 @@ Proof. simpl. reflexivity. Qed.
 
 (* SeanE *)
 Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  match b1, b2, b3 with
+  | true , true , true => true
+  | _ , _ , _ => false
+  end.
 
 Example test_andb31:                 (andb3 true true true) = true.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_andb32:                 (andb3 false true true) = false.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_andb33:                 (andb3 true false true) = false.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_andb34:                 (andb3 true true false) = false.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 (** [] *)
 
 (* ================================================================= *)
