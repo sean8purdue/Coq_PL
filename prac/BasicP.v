cdf2bb74b1f4862b4a4b3a5a9400138816c77169
Sean <sean8purdue@gmail.com>
2018-08-28 20:19:41 -0400

    add prac folder, officeHour.v

---
+++ prac/BasicP.v
@@ -0,0 +1,110 @@
+
+Inductive bool : Type :=
+  | true : bool
+  | false : bool.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1:  (orb true  false) = true.
+Proof. simpl. reflexivity.  Qed.
+Example test_orb2:  (orb false false) = false.
+Proof. simpl. reflexivity.  Qed.
+Example test_orb3:  (orb false true)  = true.
+Proof. simpl. reflexivity.  Qed.
+Example test_orb4:  (orb true  true)  = true.
+Proof. simpl. reflexivity.  Qed.
+
+(** We can also introduce some familiar syntax for the boolean
+    operations we have just defined. The [Notation] command defines a new
+    symbolic notation for an existing definition. *)
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+
+Example test_orb5:  false || false || true = true.
+Proof. simpl. reflexivity. Qed.
+
+(** _A note on notation_: In [.v] files, we use square brackets
+    to delimit fragments of Coq code within comments; this convention,
+    also used by the [coqdoc] documentation tool, keeps them visually
+    separate from the surrounding text.  In the HTML version of the
+    files, these pieces of text appear in a [different font].
+
+    The command [Admitted] can be used as a placeholder for an
+    incomplete proof.  We'll use it in exercises, to indicate the
+    parts that we're leaving for you -- i.e., your job is to replace
+    [Admitted]s with real proofs. *)
+
+(** **** Exercise: 1 star (nandb)  *)
+(** Remove "[Admitted.]" and complete the definition of the following
+    function; then make sure that the [Example] assertions below can
+    each be verified by Coq.  (Remove "[Admitted.]" and fill in each
+    proof, following the model of the [orb] tests above.) The function
+    should return [true] if either or both of its inputs are
+    [false]. *)
+
+Definition nandb (b1:bool) (b2:bool) : bool
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Example test_nandb1:               (nandb true false) = true.
+(* FILL IN HERE *) Admitted.
+Example test_nandb2:               (nandb false false) = true.
+(* FILL IN HERE *) Admitted.
+Example test_nandb3:               (nandb false true) = true.
+(* FILL IN HERE *) Admitted.
+Example test_nandb4:               (nandb true true) = false.
+(* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 1 star (andb3)  *)
+(** Do the same for the [andb3] function below. This function should
+    return [true] when all of its inputs are [true], and [false]
+    otherwise. *)
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Example test_andb31:                 (andb3 true true true) = true.
+(* FILL IN HERE *) Admitted.
+Example test_andb32:                 (andb3 false true true) = false.
+(* FILL IN HERE *) Admitted.
+Example test_andb33:                 (andb3 true false true) = false.
+(* FILL IN HERE *) Admitted.
+Example test_andb34:                 (andb3 true true false) = false.
+(* FILL IN HERE *) Admitted.
+(** [] *)
+
+(* ================================================================= *)
+(** ** Function Types *)
+
+(** Every expression in Coq has a type, describing what sort of
+    thing it computes. The [Check] command asks Coq to print the type
+    of an expression. *)
+
+Check true.
+(* ===> true : bool *)
+Check (negb true).
+(* ===> negb true : bool *)
+
+(** Functions like [negb] itself are also data values, just like
+    [true] and [false].  Their types are called _function types_, and
+    they are written with arrows. *)
+
+Check negb.
+(* ===> negb : bool -> bool *)
