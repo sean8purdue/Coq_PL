1de4f20a4aaa81da44786f81d6473877e1a92272
Sean <sean8purdue@gmail.com>
2018-08-29 21:52:38 -0400

    2 fibonacci sequence illustrate and clear the wrong understanding

--- prac/BasicP.v
+++ prac/BasicP.v
@@ -228,7 +228,6 @@ the fibonnaci function that Coq accepts as terminating.
 Example test_fibonacci1:          (fibonacci 1) = 1.
 Proof. simpl. reflexivity. Qed.
 (* Example test_fibonacci2:          (fibonacci 2) = 1.
-(* Wrong: fibonacci 2 => 2   = 1*)
 Proof. simpl. reflexivity. Qed. *)
 (* Example test_fibonacci3:          (fibonacci 3) = 2.
 Proof. simpl. reflexivity. Qed. *)
@@ -238,6 +237,23 @@ Example test_fibonacci5:          (fibonacci 5) = 5.
 Proof. simpl. reflexivity. Qed.
 Example test_fibonacci6:          (fibonacci 6) = 8.
 Proof. simpl. reflexivity. Qed. *)
+
+(* Wrong: fibonacci 2 => 2   = 1*)
+(* not Wrong *)
+(* !!! There are actually two definitions of Fibonacci sequence!
+
+In mathematics, the Fibonacci numbers are the numbers in the following integer sequence,
+called the Fibonacci sequence, and characterized by the fact that every number after the first two is the sum of the two preceding ones.
+
+1, 1, 2, 3, 5, 8...
+
+Often, especially in modern usage, the sequence is extended by one more initial term:
+
+0, 1, 1, 2, 3, 5, 8...
+
+By definition, the first two numbers in the Fibonacci sequence are 
+either 1 and 1, or 0 and 1, depending on the chosen starting point of the sequence,
+and each subsequent number is the sum of the previous two. *)
 
 (** We can make numerical expressions a little easier to read and
     write by introducing _notations_ for addition, multiplication, and
@@ -284,3 +300,28 @@ Example test_blt_nat3:             (blt_nat 4 2) = false.
 Proof. simpl. reflexivity. Qed.
 
 (** [] *)
+
+(* ################################################################# *)
+(** * Proof by Simplification *)
+
+Theorem plus_O_n : forall n : nat, 0 + n = n.
+Proof.
+  intros n. 
+  Print nat.
+  simpl. reflexivity.  Qed.
+    
+Theorem plus_O_n' : forall n : nat, 0 + n = n.
+Proof.
+  (* intros n. reflexivity. Qed. *)
+  intros n. 
+  Print nat.
+  Print simpl.
+  simpl. reflexivity. Qed.
+    
+Theorem plus_1_l : forall n:nat, 1 + n = S n.
+Proof.
+  intros n. reflexivity.  Qed.
+
+Theorem mult_0_l : forall n:nat, 0 * n = 0.
+Proof.
+  intros n. reflexivity.  Qed.
